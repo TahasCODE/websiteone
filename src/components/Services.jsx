ba79cfc480f28e130a07b3f56309c7f5685389e0
TahasCODE <=>
2024-02-13 01:37:06 +0500

    Images Changed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"
-import img1 from "../assets/3.jpg"
-import img2 from "../assets/4.jpg"
+import img1 from "../assets/3.webp"
+import img2 from "../assets/4.webp"
 
 const Services = () => {
   return (
