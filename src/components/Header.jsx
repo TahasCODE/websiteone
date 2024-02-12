15a395d8bc2148db545f122e89c5b556b8e25576
TahasCODE <=>
2024-02-13 02:54:50 +0500

    Littl h1 change in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { HashLink } from 'react-router-hash-link'
 
 const Header = () => {
   return <nav>
-    <h1>TechyStar.</h1>
+    <h1>Taha's TechyStar.</h1>
     <main>
         <HashLink to={"/#home"}>Home</HashLink>
         <Link to={"/contact"}>Contact</Link>
